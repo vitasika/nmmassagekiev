4fa02e7d678d60f33bc711d433361769572006f6
vitasika <vitasika1@gmail.com>
2023-01-24 15:56:41 +0000

    Deploying to gh-pages from @ vitasika/nmmassagekiev_new@043448f6154e3641a8bfef4302f3286a80e8ffb8 🚀

---
+++ index.18835d83.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-modal-open]"),t=document.querySelector("[data-modal-close]"),d=document.querySelector("[data-modal]");function c(){d.classList.toggle("is-hidden")}e.addEventListener("click",c),t.addEventListener("click",c);
+//# sourceMappingURL=index.18835d83.js.map
