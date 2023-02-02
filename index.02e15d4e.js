5cc4d451d9dc3830f39bc5ebde733731b455906c
vitasika <vitasika1@gmail.com>
2023-02-02 23:40:07 +0000

    Deploying to gh-pages from @ vitasika/nmmassagekiev_new@f1470ce4e4b0955cdfe9a97835736225dde7a436 🚀

---
+++ index.02e15d4e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");console.log(e),e.addEventListener("click",(()=>{const o="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!o),console.log(e),t.classList.toggle("is-open")}));
+//# sourceMappingURL=index.02e15d4e.js.map
---
+++ index.8f94583a.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");console.log(e),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),console.log(e),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.8f94583a.js.map
