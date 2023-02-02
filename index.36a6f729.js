e5b636c6bb8c153f10d999f911b67fe8438c85db
vitasika <vitasika1@gmail.com>
2023-02-02 12:36:30 +0000

    Deploying to gh-pages from @ vitasika/nmmassagekiev_new@fc18e0fcda1ddf6d6b74be8bbf39d032e2fa35c6 🚀

--- index.36a6f729.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("[data-menu-button]");console.log(o);
-//# sourceMappingURL=index.36a6f729.js.map
--- index.54dba428.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("[data-menu-button]");console.log(o)}();
-//# sourceMappingURL=index.54dba428.js.map
