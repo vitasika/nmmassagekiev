2dc5d55fa8b873eeaaec1f6b70daf70dcb226eec
Vitalij Shcherbak <vitasika1@gmail.com>
2023-02-03 00:59:06 +0200

    mobil open

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -4,7 +4,12 @@ const menuBtn = document.querySelector("[data-menu-button]")
 console.log(menuBtn);
 
 menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("is-open")
+    const expended = menuBtn.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtn.classList.toggle("is-open");
+    menuBtn.setAttribute("aria-expanded", !expended);
+    console.log(menuBtn);
+
 })
 
 
