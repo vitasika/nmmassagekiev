89fb44f4f0930bac202b0eb926024bc08af4754c
Vitalij Shcherbak <vitasika1@gmail.com>
2023-02-04 22:44:39 +0200

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,13 +3,9 @@ const menuBtn = document.querySelector("[data-menu-button]");
 const mobileMenu = document.querySelector("[data-menu]");
 const mobileMenuClosed = document.querySelector("[data-menu-closed]");
 
-console.log(mobileMenuClosed);
-
 menuBtn.addEventListener("click", () => {
     const expended = menuBtn.getAttribute("aria-expanded") === "true" || false;
-
     menuBtn.classList.toggle("is-open");
-
     menuBtn.setAttribute("aria-expanded", !expended);
 
     mobileMenu.classList.toggle("is-open");
@@ -18,41 +14,9 @@ menuBtn.addEventListener("click", () => {
 
 mobileMenuClosed.addEventListener("click", () => {
     const expended = menuBtn.getAttribute("aria-expanded") === false || "true";
-
     menuBtn.classList.remove("is-open");
-
     menuBtn.setAttribute("aria-expanded", !expended);
 
     mobileMenu.classList.remove("is-open");
     
 })
-
-// (() => {
-//     const mobileMenu = document.querySelector('.js-menu-container');
-//     const openMenuBtn = document.querySelector('.js-open-menu');
-//     const closeMenuBtn = document.querySelector('.js-close-menu');
-
-//     const toggleMenu = () => {
-//         const isMenuOpen =
-//             openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//         openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//         mobileMenu.classList.toggle('is-open');
-
-//         const scrollLockMethod = !isMenuOpen
-//             ? 'disableBodyScroll'
-//             : 'enableBodyScroll';
-//         bodyScrollLock[scrollLockMethod](document.body);
-//     };
-
-//     openMenuBtn.addEventListener('click', toggleMenu);
-//     closeMenuBtn.addEventListener('click', toggleMenu);
-
-//     // Закрываем мобильное меню на более широких экранах
-//     // в случае изменения ориентации устройства.
-//     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-//         if (!e.matches) return;
-//         mobileMenu.classList.remove('is-open');
-//         openMenuBtn.setAttribute('aria-expanded', false);
-//         bodyScrollLock.enableBodyScroll(document.body);
-//     });
-// })();
